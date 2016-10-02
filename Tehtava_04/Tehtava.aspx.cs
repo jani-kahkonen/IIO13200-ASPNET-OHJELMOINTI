edd853e2d1421a071b8644719d981f7afbc226e3
jani <H9575@student.jamk.fi>
2016-10-02 23:53:17 +0300

    Tehtava_04 valmis.

---
+++ Tehtava_04/Tehtava.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Tehtava : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            GridView_2.DataSource = BLTehtava.get_data();
+
+            GridView_2.DataBind();
+        }
+        catch (Exception ex)
+        {
+            lblError.Text = ex.Message;
+        }
+    }
+}
