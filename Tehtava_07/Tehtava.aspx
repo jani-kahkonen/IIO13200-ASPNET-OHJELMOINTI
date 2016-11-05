3a5f61676a6a2decbae806735364333b9d33ee51
Jani <H9575@student.jamk.fi>
2016-11-06 00:05:56 +0200

    Tehtava_07 valmis.

---
+++ Tehtava_07/Tehtava.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Tehtava.aspx.cs" Inherits="Tehtava" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head runat="server">
+        <title>Tehtava_07</title>
+        <link href="Styles/Styles.css" rel="stylesheet" type="text/css" />
+    </head>
+    
+    <body>
+        <form id="form" runat="server">
+            <h1>Tehtava_07</h1>
+
+            <div>
+                <asp:Label runat="server" Text="PVM:" CssClass="label" />
+                <asp:TextBox ID="txtDate" runat="server" />
+                <br />
+                <asp:Label runat="server" Text="Nimi:" CssClass="label" />
+                <asp:TextBox ID="txtName" runat="server" />
+                <br />
+                <asp:Label runat="server" Text="Koodi:" CssClass="label" />
+                <asp:TextBox ID="txtCourse" runat="server" />
+                <br />
+                <asp:Label runat="server" Text="Olen oppinut:" CssClass="label" />
+                <asp:TextBox ID="txtEvaluationA" runat="server" TextMode="multiline" />
+                <br />
+                <asp:Label runat="server" Text="Haluan oppia:" CssClass="label" />
+                <asp:TextBox ID="txtEvaluationB" runat="server" TextMode="multiline" />
+            </div>
+
+            <div>
+                <asp:Label runat="server" Text="Hyvää:" CssClass="label" />
+                <asp:TextBox ID="txtFeedbackA" runat="server" TextMode="multiline" />
+                <br />
+                <asp:Label runat="server" Text="Parannettavaa:" CssClass="label" />
+                <asp:TextBox ID="txtFeedbackB" runat="server" TextMode="multiline" />
+                <br />
+                <asp:Label runat="server" Text="Muuta:" CssClass="label" />
+                <asp:TextBox ID="txtFeedbackC" runat="server" TextMode="multiline" />
+                <br />
+                <asp:CustomValidator ID="myCustomValidator" runat="server" ErrorMessage="CustomValidator" OnServerValidate="myCustomValidator_ServerValidate" />
+                <br />
+                <asp:Button ID="btnSendXML" runat="server" Text="Tallenna XML" CssClass="button" OnClick="btnSendXML_Click" />
+                <asp:Button ID="btnSendSQL" runat="server" Text="Tallenna SQL" CssClass="button" OnClick="btnSendSQL_Click" />
+            </div>
+
+            <div>
+                <h1>Palautteet</h1>
+                <asp:Button ID="btnLoadXML" runat="server" Text="Hae XML" CssClass="button" OnClick="btnLoadXML_Click" />
+                <asp:Button ID="btnLoadSQL" runat="server" Text="Hae SQL" CssClass="button" OnClick="btnLoadSQL_Click" />
+
+                <asp:GridView ID="gvFeedback" runat="server" />
+            </div>
+
+            <asp:Label ID="lblMessage" runat="server" />
+        </form>
+    </body>
+</html>
---
+++ Tehtava_07/Tehtava.aspx.cs
@@ -0,0 +1,144 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+public partial class Tehtava : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            DateTime today = DateTime.Today;
+
+            txtDate.Text = today.ToString("yyyy.MM.dd");
+        }
+    }
+
+    protected void btnSendXML_Click(object sender, EventArgs e)
+    {
+        if (txtDate.Text != string.Empty && txtName.Text != string.Empty && txtCourse.Text != string.Empty)
+        {
+            string connection_string = ConfigurationManager.AppSettings["XML"];
+
+            try
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(Server.MapPath(connection_string));
+
+                XmlNode rootNode = xmlDocument.SelectSingleNode("palautteet");
+                XmlNode xmlNode = rootNode.AppendChild(xmlDocument.CreateNode(XmlNodeType.Element, "palaute", ""));
+
+                xmlNode.AppendChild(xmlDocument.CreateNode(XmlNodeType.Element, "pvm", "")).InnerText = txtDate.Text;
+                xmlNode.AppendChild(xmlDocument.CreateNode(XmlNodeType.Element, "tekija", "")).InnerText = txtName.Text;
+                xmlNode.AppendChild(xmlDocument.CreateNode(XmlNodeType.Element, "koodi", "")).InnerText = txtCourse.Text;
+                xmlNode.AppendChild(xmlDocument.CreateNode(XmlNodeType.Element, "opittu", "")).InnerText = txtEvaluationA.Text;
+                xmlNode.AppendChild(xmlDocument.CreateNode(XmlNodeType.Element, "haluanoppia", "")).InnerText = txtEvaluationB.Text;
+                xmlNode.AppendChild(xmlDocument.CreateNode(XmlNodeType.Element, "hyvaa", "")).InnerText = txtFeedbackA.Text;
+                xmlNode.AppendChild(xmlDocument.CreateNode(XmlNodeType.Element, "parannettavaa", "")).InnerText = txtFeedbackB.Text;
+                xmlNode.AppendChild(xmlDocument.CreateNode(XmlNodeType.Element, "muuta", "")).InnerText = txtFeedbackC.Text;
+
+                xmlDocument.Save(Server.MapPath(connection_string));
+            }
+            catch (Exception ex)
+            {
+
+                lblMessage.Text = ex.Message;
+            }
+        }
+    }
+
+    protected void btnSendSQL_Click(object sender, EventArgs e)
+    {
+        if (txtDate.Text != string.Empty && txtName.Text != string.Empty && txtCourse.Text != string.Empty)
+        {
+            string connection_string = ConfigurationManager.ConnectionStrings["SQL"].ConnectionString;
+
+            try
+            {
+                MySqlConnection mySqlConnection = new MySqlConnection(connection_string);
+                mySqlConnection.Open();
+
+                MySqlCommand mySqlCommand = mySqlConnection.CreateCommand();
+                mySqlCommand.CommandText = "INSERT INTO palaute(pvm, tekija, opintojakso, opittu, haluanoppia, hyvaa, parannettavaa, muuta) VALUES(@pvm, @tekija, @opintojakso, @opittu, @haluanoppia, @hyvaa, @parannettavaa, @muuta)";
+                mySqlCommand.Parameters.AddWithValue("@pvm", txtDate.Text);
+                mySqlCommand.Parameters.AddWithValue("@tekija", txtName.Text);
+                mySqlCommand.Parameters.AddWithValue("@opintojakso", txtCourse.Text);
+                mySqlCommand.Parameters.AddWithValue("@opittu", txtEvaluationA.Text);
+                mySqlCommand.Parameters.AddWithValue("@haluanoppia", txtEvaluationB.Text);
+                mySqlCommand.Parameters.AddWithValue("@hyvaa", txtFeedbackA.Text);
+                mySqlCommand.Parameters.AddWithValue("@parannettavaa", txtFeedbackB.Text);
+                mySqlCommand.Parameters.AddWithValue("@muuta", txtFeedbackC.Text);
+                mySqlCommand.ExecuteNonQuery();
+
+                mySqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+
+                lblMessage.Text = ex.Message;
+            }
+        }
+    }
+
+    protected void myCustomValidator_ServerValidate(object source, ServerValidateEventArgs args)
+    {
+        if (txtDate.Text == string.Empty || txtName.Text == string.Empty || txtCourse.Text == string.Empty)
+        {
+            myCustomValidator.ErrorMessage = string.Format("Täytä tarvittavat tiedot.");
+
+            args.IsValid = false;
+        }
+        else
+        {
+            myCustomValidator.ErrorMessage = string.Empty;
+
+            args.IsValid = true;
+        }
+    }
+
+    protected void btnLoadXML_Click(object sender, EventArgs e)
+    {
+        string connection_string = ConfigurationManager.AppSettings["XML"];
+
+        try
+        {
+            DataSet dataSet = new DataSet();
+            dataSet.ReadXml(Server.MapPath(connection_string));
+
+            gvFeedback.DataSource = dataSet;
+            gvFeedback.DataBind();
+        }
+        catch (Exception ex)
+        {
+            lblMessage.Text = ex.Message;
+        }
+    }
+
+    protected void btnLoadSQL_Click(object sender, EventArgs e)
+    {
+        string connection_string = ConfigurationManager.ConnectionStrings["SQL"].ConnectionString;
+
+        try
+        {
+            MySqlConnection mySqlConnection = new MySqlConnection(connection_string);
+            mySqlConnection.Open();
+
+            MySqlDataAdapter mySqlDataAdapter = new MySqlDataAdapter(new MySqlCommand("SELECT * FROM palaute;", mySqlConnection));
+
+            DataSet dataSet = new DataSet();
+            mySqlDataAdapter.Fill(dataSet);
+
+            gvFeedback.DataSource = dataSet;
+            gvFeedback.DataBind();
+
+            mySqlConnection.Close();
+        }
+        catch (Exception ex)
+        {
+            lblMessage.Text = ex.Message;
+        }
+    }
+}
